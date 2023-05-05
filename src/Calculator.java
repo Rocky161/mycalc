edd933c1847f5b14b7cf629e0ee5a679bfb24dde
Rocky161 <132611280+Rocky161@users.noreply.github.com>
2023-05-05 22:25:16 +0300

    mycalc
    maycalc

---
+++ src/Calculator.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Calculator{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        Main result = new Main();
+        System.out.println("Input:");
+        String expression = input.nextLine();
+        String answer = result.calc(expression);
+
+
+        System.out.println("Output:\n" + answer);
+    }
+}
+
+
